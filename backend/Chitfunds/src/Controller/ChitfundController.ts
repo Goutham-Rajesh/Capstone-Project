a79f209069f0d336f46bb93c7714cae098782348
Goutham-Rajesh <gouthamrajesh5355@gmail.com>
2024-10-26 12:08:53 +0530

    updated the controller

--- backend/Chitfunds/src/Controller/ChitfundController.ts
+++ backend/Chitfunds/src/Controller/ChitfundController.ts
@@ -31,4 +31,28 @@ const  createChitfund = async(req:Request, res:Response)=>{
     }
 }
 
-export {getChitfunds, getChitfundById, createChitfund};
+const updateChitfundById = async(req:Request, res:Response)=>{
+    const {participantId} = req.body;
+    try{
+        const updatedChitfund = await Chitfunds.findByIdAndUpdate(
+            req.params.id,
+            { $addToSet: { Participants: participantId } },
+            { new: true }
+        );
+        res.status(200).json(updatedChitfund);
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+}
+
+const getchitfundByParticipantId = async(req:Request, res:Response)=>{
+    try{
+        const Chitfund = await Chitfunds.find({Participants:req.params.id});
+        res.status(200).json(Chitfund);
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+}
+
+
+export {getChitfunds, getChitfundById, createChitfund,getchitfundByParticipantId,updateChitfundById};
--- backend/Chitfunds/src/Routes/ChitfundRoutes.ts
+++ backend/Chitfunds/src/Routes/ChitfundRoutes.ts
@@ -1,4 +1,4 @@
-import { getChitfunds, getChitfundById , createChitfund } from "../Controller/ChitfundController";
+import { getChitfunds, getChitfundById , createChitfund, getchitfundByParticipantId, updateChitfundById } from "../Controller/ChitfundController";
 import express from "express";
 
 const router = express.Router();
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get("/getchitfunds", getChitfunds);
 router.get("/getchitfundbyid/:id", getChitfundById);
 router.post("/createChitfund", createChitfund);
+router.get("/getchitfundbyparticipantid/:id", getchitfundByParticipantId);
+router.get("/updatechitfundbyid/:id", updateChitfundById);
 
 export default router;
