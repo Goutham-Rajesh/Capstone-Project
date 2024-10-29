67c1a6c58ad0aa4ed4902c776608fbad87f566b1
Goutham-Rajesh <gouthamrajesh5355@gmail.com>
2024-10-29 13:13:18 +0530

    updated files

---
+++ frontend/chit-app/src/pages/UserLoggedIn.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+import UserChitFund from "../components/UserChitFund";
+
+function UserLoggedIn(){
+    return(
+        <div>
+            <NavBar/>
+            <UserChitFund/>
+        </div>
+    )
+}
+
+export default UserLoggedIn;
